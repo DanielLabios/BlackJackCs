49d0da0d9e05a0894156f99d861f3f3ffa5c6442
Daniel Labios <labiosd@gmail.com>
2020-08-14 22:37:32 -0400

    Deck Creation has been coded & verified

--- Program.cs
+++ Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BlackJackCs
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Card
     {
-      Console.WriteLine("Welcome to C#");
+        public string suite { get; set; }
+        public string rank { get; set; }
+        public int cardValue { get; set; }
     }
-  }
+
+    class Program
+    {
+        static int GiveValue(int rank)
+        {
+            if (rank == 0)
+            {
+                return 11;
+            }
+            else if (rank >= 10)
+            {
+                return 10;
+            }
+            else
+                return rank + 1;
+        }
+        static void Main(string[] args)
+        {
+
+            // Creating The Deck of Cards
+
+            var deck = new List<Card>();
+            for (var indexCount = 0; indexCount < 4; indexCount++)
+            {
+                var suite = new List<string> { "Diamonds", "Spades", "Hearts", "Clubs" };
+                var rank = new List<string>();
+                rank.Add("Ace");
+                for (var counter = 2; counter <= 10; counter++)
+                {
+                    rank.Add($"{counter}");
+                }
+                rank.AddRange(new string[] { "Jack", "Queen", "King" });
+                for (var suiteSetCounter = 0; suiteSetCounter < 13; suiteSetCounter++)
+                {
+                    var newCard = new Card()
+                    {
+                        suite = suite[indexCount],
+                        rank = rank[suiteSetCounter],
+                        cardValue = GiveValue(suiteSetCounter),
+                    };
+                    deck.Add(newCard);
+                }
+            }
+            /* Checking Deck of Cards (To see if it is correct)
+            int cardCount = 0;
+            foreach (Card card in deck)
+            {
+                Console.WriteLine($"{deck[cardCount].rank} of {deck[cardCount].suite}");
+                Console.WriteLine(deck[cardCount].cardValue);
+                cardCount++;
+            }*/
+
+
+
+
+
+
+        }
+    }
 }
