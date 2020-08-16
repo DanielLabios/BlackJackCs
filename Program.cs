413eb8372e68f7b4208ef6b9058243879346ee33
Daniel Labios <labiosd@gmail.com>
2020-08-15 22:29:35 -0400

    WIP Deal Cards

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@ namespace BlackJackCs
         public string rank { get; set; }
         public int cardValue { get; set; }
     }
+    class Deck
+    {
+        public List<Card> collection { get; set; }
+        public int deckCounter { get; set; }
+    }
 
+    class Hand
+    {
+        public List<Card> cardsInHand { get; set; }
+        public int handValue { get; set; }
+    }
     class Program
     {
         static int GiveValue(int rank)
@@ -25,22 +35,35 @@ namespace BlackJackCs
             else
                 return rank + 1;
         }
+
+        static int Deal(Hand Player, Deck inPlay, int numberOfCards)
+        {
+            int counter = 0;
+            while (counter < numberOfCards)
+            {
+                Player.cardsInHand.Add(inPlay.collection[inPlay.deckCounter + counter]);
+                counter++;
+            }
+            return inPlay.deckCounter = inPlay.deckCounter + numberOfCards;
+        }
+
         static void Main(string[] args)
         {
 
-            // Creating The Deck of Cards
+            // Creating The Deck of Cards + Player + Dealer
 
             var deck = new List<Card>();
+            var suite = new List<string> { "Diamonds", "Spades", "Hearts", "Clubs" };
+            var rank = new List<string>();
+            rank.Add("Ace");
+            for (var counter = 2; counter <= 10; counter++)
+            {
+                rank.Add($"{counter}");
+            }
+            rank.AddRange(new string[] { "Jack", "Queen", "King" });
+
             for (var indexCount = 0; indexCount < 4; indexCount++)
             {
-                var suite = new List<string> { "Diamonds", "Spades", "Hearts", "Clubs" };
-                var rank = new List<string>();
-                rank.Add("Ace");
-                for (var counter = 2; counter <= 10; counter++)
-                {
-                    rank.Add($"{counter}");
-                }
-                rank.AddRange(new string[] { "Jack", "Queen", "King" });
                 for (var suiteSetCounter = 0; suiteSetCounter < 13; suiteSetCounter++)
                 {
                     var newCard = new Card()
@@ -52,20 +75,38 @@ namespace BlackJackCs
                     deck.Add(newCard);
                 }
             }
-            /* Checking Deck of Cards (To see if it is correct)
+
+            var player = new Hand();
+            var dealer = new Hand();
+
+            // Shuffle The Deck
+
+            for (int shuffleCount = deck.Count; shuffleCount > 1; shuffleCount--)
+            {
+                var randomNumberGenerator = new Random();
+                var randomNumber = randomNumberGenerator.Next(51);
+                var swapContainer = deck[shuffleCount - 1];
+                deck[shuffleCount - 1] = deck[randomNumber];
+                deck[randomNumber] = swapContainer;
+            }
+            Deal(player, deck, 2);
+
+
+
+            /*/Checking Deck of Cards To see if it is correct
             int cardCount = 0;
             foreach (Card card in deck)
             {
                 Console.WriteLine($"{deck[cardCount].rank} of {deck[cardCount].suite}");
                 Console.WriteLine(deck[cardCount].cardValue);
-                cardCount++;
-            }*/
-
-
-
-
-
-
+                cardCount++;*/
         }
+
+
+
+
+
+
     }
 }
+
